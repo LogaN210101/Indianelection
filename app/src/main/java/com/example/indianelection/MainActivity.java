9c2372bca6d94c6773b5f58e0181cb38dee974f7
Tamojit Basu <tamojit2001@gmail.com>
2020-05-20 15:15:41 +0530

    Basic

--- app/src/main/java/com/example/indianelection/MainActivity.java
+++ app/src/main/java/com/example/indianelection/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.indianelection;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b = findViewById(R.id.button2);
+        b.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == b)
+        {
+            Intent i= new Intent(MainActivity.this, Main2Activity.class);
+            startActivity(i);
+
+        }
+
     }
 }
